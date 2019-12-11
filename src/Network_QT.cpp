dcfc26fe791551ed744be1360c3ef509629568c8
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2019-12-11 22:27:00 +0800

    Network rewrite complete
    Use Network_QT instead

---
+++ src/Network_QT.cpp
@@ -0,0 +1,165 @@
+#include <Network_QT.h>
+Network_QT* Network_QT::network_QT = nullptr;
+
+bool Network_QT::networkInit() {
+	getInstance();
+	stopFlag = false;
+	listener = new QTcpServer();
+	if (!listener->listen(QHostAddress::Any, Setting::getSetting()->getListenPort())) {
+		return false;
+	}
+	QObject::connect(listener, &QTcpServer::newConnection, this, &Network_QT::ConnectHandler);
+	//open listen port
+}
+
+void Network_QT::ConnectHandler() {
+	QTcpSocket* s = this->listener->nextPendingConnection();
+
+	while (!s->canReadLine()) {
+
+	}
+	if (s->readLine() != "1919810") {
+		s->close();
+		return;
+	}
+	std::string temp = "";
+	while (temp == "") {
+		temp = s->readLine();
+	}
+
+	json callerInfo;
+	try {
+		callerInfo = json::parse(temp);
+		//TODO: Check Black lsit
+	}
+	catch (int e) {
+		s->close();
+		return;
+	}
+
+	//TODO: Accept?
+
+	json sendTemp;
+	sendTemp["ID"]["Accept"] = true;
+	sendTemp["ID"]["name"] = Setting::getSetting()->getName();
+	sendTemp["ID"]["profile_photo"] = Mat2Base64(Setting::getSetting()->getProfile(), "jpg");//?
+	sendTemp["data"]["model_id"] = Setting::getSetting()->getModelID();
+	sendTemp["data"]["session_id"] = GenerateGuid();
+	s->write(sendTemp.dump().c_str(), sendTemp.dump().length());
+
+	MotionObject* motion = new MotionObject(sendTemp["data"]["session_id"]);
+	Network_QT::getInstance()->getDisplayObjects()->push_back(motion);
+	motion->setProfileByBase64(callerInfo["ID"]["profile_photo"]);
+	if (callerInfo["ID"]["name"].is_string())
+		motion->name = callerInfo["ID"]["name"].get<std::string>();
+	else
+		motion->name = "UNKNOW";
+
+	json sendJson;
+	sendJson["ID"]["session_id"] = sendTemp["data"]["session_id"];
+	//sendJson["ID"]["send_time"] = Necessary?
+	sendJson["data"] = *Network_QT::getInstance()->getSendJson();
+	s->write(sendTemp.dump().c_str(), sendTemp.dump().length());
+
+	QThread* newCallThd = new QThread(this);
+	CallObj* newCall = new CallObj(motion, s);
+	Network_QT::getInstance()->calls.push_back(newCallThd);
+	newCallThd->start();
+	newCall->moveToThread(newCallThd);
+	
+
+	//TODO: Some finishing work
+	motion->alive = false;
+}
+
+void Network_QT::updateMotion(json& motion)
+{
+	this->sendObject.erase("data");
+	this->sendObject.emplace("data", motion);
+}
+
+std::vector<MotionObject*>* Network_QT::getDisplayObjects()
+{
+	return &this->displayObjects;
+}
+
+json* Network_QT::getSendJson()
+{
+	return &sendObject;
+}
+
+void Network_QT::call(std::string ip, int port){
+	QTcpSocket* caller = new QTcpSocket();
+	caller->connectToHost(QString::fromStdString(ip), port);
+	caller->write("1919810");
+	if (caller->readLine() != "114514") {
+		caller->close();
+		throw "NO_RESPONSE";
+	}
+
+	json exchange;
+	exchange["ID"]["name"] = Setting::getSetting()->getName();
+	exchange["ID"]["name"] = Mat2Base64(Setting::getSetting()->getProfile(), Setting::getSetting()->getProfileType());
+
+	if (getHostMacAddress()!="")
+		exchange["ID"]["MAC"] = getHostMacAddress();
+	else
+		exchange["ID"]["MAC"] = "UNKNOWN";
+
+	exchange["data"]["model_id"] = Setting::getSetting()->getModelID();
+	caller->write(exchange.dump().c_str(),exchange.dump().length());
+	json info = json::parse(caller->readLine());
+	if (info["ID"]["Accept"]) {
+		MotionObject* thisCall = new MotionObject(info["data"]["session_id"]);
+		thisCall->setProfileByBase64(info["ID"]["profile_photo"]);
+		if (info["ID"]["name"].is_string())
+			thisCall->name = info["ID"]["name"].get<std::string>();
+		else
+			thisCall->name = "UNKNOW";
+
+		Network_QT::getInstance()->getDisplayObjects()->push_back(thisCall);
+		CallObj* newCallObj = new CallObj(thisCall, caller);
+		QThread* newCallThd = new QThread();
+		newCallThd->start();
+		newCallObj->moveToThread(newCallThd);
+		Network_QT::getInstance()->calls.push_back(newCallThd);
+	}
+}
+
+
+void Network_QT::call(std::string target){//IPv4 Only
+	if (target.find_first_of(':') == std::string::npos) {
+		if (target.find_first_of('£º') == std::string::npos) {
+			throw "BAD_ADDRESS";
+		}
+		else
+		{
+			target[target.find_first_of('£º')] = ':';
+		}
+	}
+	try {
+		call(target.substr(target.find_first_of(':')), std::stoi(target.substr(target.find_first_of(':') + 1, target.length() - target.find_first_of(':') - 1)));
+	}
+	catch (std::string e) {
+		throw "BAD_ADDRESS";
+	}
+}
+
+Network_QT::Network_QT() {
+	stopFlag = false;
+	listener = nullptr;
+	sendObject["ID"];
+	sendObject["data"];
+
+}
+
+std::string GenerateGuid() {
+	return QUuid::createUuid().toString().toStdString();
+}
+
+CallObj::CallObj(MotionObject* motion, QTcpSocket* s) {
+	this->motion = motion;
+	this->s = s;
+	QObject::connect(s, &QTcpSocket::readyRead, this, &CallObj::writeObject);
+	QObject::connect(FacialLandmarkDetector::getInstance(), &FacialLandmarkDetector::NewDetection, this, &CallObj::sendObject);
+}
---
+++ src/helper.cpp
@@ -0,0 +1,41 @@
+#include <helper.hpp>
+
+std::string getHostMacAddress()
+{
+    QList<QNetworkInterface> nets = QNetworkInterface::allInterfaces();// 获取所有网络接口列表
+    int nCnt = nets.count();
+    QString strMacAddr = "";
+    for (int i = 0; i < nCnt; i++)
+    {
+        // 如果此网络接口被激活并且正在运行并且不是回环地址，则就是我们需要找的Mac地址
+        if (nets[i].flags().testFlag(QNetworkInterface::IsUp) && nets[i].flags().testFlag(QNetworkInterface::IsRunning) && !nets[i].flags().testFlag(QNetworkInterface::IsLoopBack))
+        {
+            strMacAddr = nets[i].hardwareAddress();
+            break;
+        }
+    }
+    return strMacAddr.toStdString();
+}
+//————————————————
+//版权声明：本文为CSDN博主「dofaster」的原创文章，遵循 CC 4.0 BY - SA 版权协议，转载请附上原文出处链接及本声明。
+//原文链接：https ://blog.csdn.net/llmys/article/details/52861545
+
+
+
+inline double euclideanDistance(double x1, double y1, double x2, double y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
+template<typename T>
+struct euclideanPoint {
+    T x;
+    T y;
+};
+
+template<typename T>
+inline euclideanPoint<T> rotate(T x, T y, double theta) {
+    euclideanPoint<T> temp;
+    temp.x = cos(theta) * x + sin(theta) * y;
+    temp.y = cos(theta) * y - sin(theta) * x;
+    return temp;
+}
